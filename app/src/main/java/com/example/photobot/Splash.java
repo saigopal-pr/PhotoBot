cbde9450af08f358978b93c16e3e2806ba941799
saigopal-pr <saigopal.pr@gmail.com>
2023-05-30 22:02:48 -0700

    done

---
+++ app/src/main/java/com/example/photobot/Splash.java
@@ -0,0 +1,32 @@
+package com.example.photobot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_DELAY);
+    }
+}
+
+
+
+
+
