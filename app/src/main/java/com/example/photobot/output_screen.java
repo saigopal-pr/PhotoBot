1fc4eb6de73de714c4b24f984bf7ffb4c952b7eb
saigopal-pr <saigopal.pr@gmail.com>
2023-06-06 08:17:16 -0700

    done

--- app/src/main/java/com/example/photobot/output_screen.java
+++ app/src/main/java/com/example/photobot/output_screen.java
@@ -3,12 +3,19 @@ package com.example.photobot;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class output_screen extends AppCompatActivity {
     private TextView questionoutput;
     private TextView answeroutput;
+
+    private Button back;
+    private Activity context;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,14 @@ public class output_screen extends AppCompatActivity {
         String answer = getIntent().getStringExtra("answer");
         questionoutput.setText(question);
         answeroutput.setText(answer);
+        back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Intent intent = new Intent(output_screen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
